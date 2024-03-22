4bb25a453f883b34b1e048cb28a0920df36f67e6
liqman <48323002+liqman@users.noreply.github.com>
2024-03-22 10:54:56 +0800

    Update config.py
    Signed-off-by: liqman <48323002+liqman@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,22 +1,20 @@
 source_file = "demo.txt"
 final_file = "result.txt"
 favorite_list = [
-    "珠江",
-    "开平综合",
-    "开平生活",
+    "南京",
+    "苏州",
+    "江苏",
     "CCTV1",
     "CCTV5",
     "CCTV5+",
     "CCTV13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
     "浙江卫视",
+    "江苏卫视",
     "湖南卫视",
     "翡翠台",
 ]
-favorite_page_num = 5
-default_page_num = 3
+favorite_page_num = 4
+default_page_num = 2
 urls_limit = 15
 response_time_weight = 0.5
 resolution_weight = 0.5
